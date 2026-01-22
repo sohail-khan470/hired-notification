618d117654073708be10b0c27826da8e7f17b909
sohail khan <sohail@bluell.se>
2026-01-22 09:24:23 +0500

    fixed the queue name

--- src/constants/queue.js
+++ src/constants/queue.js
@@ -1,4 +1,4 @@
 module.exports = {
-  AUTH_EMAIL_QUEUE: "notifications-service.auth.email",
-  ORDER_EMAIL_QUEUE: "notifications-service.order.email",
+  AUTH_EMAIL_QUEUE: "notifications-service.auth.email.queue",
+  ORDER_EMAIL_QUEUE: "notifications-service.order.email.queue",
 };
